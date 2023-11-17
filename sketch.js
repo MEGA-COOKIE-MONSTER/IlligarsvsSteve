862d48e52fe086570bd7f7b88b48099d06401337
MEGA-COOKIE-MONSTER <120087497+MEGA-COOKIE-MONSTER@users.noreply.github.com>
2023-11-17 19:25:52 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var steve, steveAni
+
+function setup(){
+
+}
+
+function preload(){
+ steveAni = loadAnimation("steveWalkingfr1.png", )
+}
+
+function draw(){
+
+}
+//find or make bg
+//steve walking frame 2
+//steve attaking frames
+//illigar shooting frames 
+//steve blocking frame
+//arrow
+//winning bg
